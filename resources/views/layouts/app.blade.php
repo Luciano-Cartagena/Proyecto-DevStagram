bc80611db2bb9d948246e8e284b86be4d9d7c79e
Luciano <Lucianocartagena17@gmail.com>
2024-09-25 21:03:53 -0300

    Autenticación de Usuario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,28 @@
                 <h1 class="text-3xl font-black">
                 DevStagram
                 </h1>
-                <nav class="flex gap-2 items-center">
-                    <a class="font-bold uppercase text-gray-600" href="{{route('login')}}">Login</a>
-                    <a class="font-bold uppercase text-gray-600" href="{{ route('register') }}">Crear Cuenta</a>
-                </nav>
+
+                @auth
+                    <nav class="flex gap-2 items-center">
+                        <a class="font-bold text-gray-600 text-sm" href="#">
+                            Hola: 
+                            <span class="font-normal"> 
+                                {{auth()->user()->username}}
+                            </span>
+                        </a>
+                        <a href="{{ route('register') }}" class="font-bold uppercase text-gray-600 text-sm">
+                            Cerrar Sesión
+                        </a>
+                    </nav>
+                @endauth
+
+                @guest
+                    <nav class="flex gap-2 items-center">
+                        <a class="font-bold uppercase text-gray-600" href="{{route('login')}}">Login</a>
+                        <a class="font-bold uppercase text-gray-600" href="{{ route('register') }}">Crear Cuenta</a>
+                    </nav>
+                @endguest
+                
             </div>
         </header>
 

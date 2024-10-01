7495f2c95f14781231ad6242e326ef6a88ffd282
Luciano <Lucianocartagena17@gmail.com>
2024-10-01 12:04:18 -0300

    Instalando Dropzone y Intervention

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @stack('styles')
         <title>DevStagram - @yield('titulo')</title>
         @vite (['resources/css/app.css', 'resources/js/app.js'])
     </head>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,11 +4,16 @@
     Crea una nueva publicación
 @endsection
 
+@push('styles')
+<link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+@endpush
+
 @section('contenido')
     <div class="md:flex justify-center md:gap-10 md:items-center">
         <div class="md:w-6/12 p-5">
-            <form id="dropzone" class=" dropzone border-dashed border-2 w-full h-96 rounded flex flex-col 
+            <form action="{{route ('imagenes.store')}}" method="POST" enctype="multipart/form-data" id="dropzone" class=" dropzone border-dashed border-2 w-full h-96 rounded flex flex-col 
             justify-center items-center ">
+            @csrf
         </form>
         </div>
 
